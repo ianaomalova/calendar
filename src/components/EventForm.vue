0bcfa6ce0fbaa398756b15a851e0907111c497da
Iana <malovayai@gmail.com>
2023-03-26 19:03:17 +0300

    init

---
+++ src/components/EventForm.vue
@@ -0,0 +1,143 @@
+<template>
+    <div class="event-form-modal">
+      <div class="event-form-wrapper">
+        <form @submit.prevent="submitEvent" class="event-form">
+          <div class="form-group">
+            <label for="title">Title:</label>
+            <input v-model="event.title" type="text" id="title" required>
+          </div>
+          <div class="form-group">
+            <label for="dateStart">Start date:</label>
+            <input v-model="event.dateStart" type="date" id="dateStart" required>
+          </div>
+          <div class="form-group">
+            <label for="dateEnd">End date:</label>
+            <input v-model="event.dateEnd" type="date" id="dateEnd" required>
+          </div>
+          <div class="form-group">
+            <label for="timeStart">Start time:</label>
+            <input v-model="event.timeStart" type="time" id="timeStart" required>
+          </div>
+          <div class="form-group">
+            <label for="timeEnd">End time:</label>
+            <input v-model="event.timeEnd" type="time" id="timeEnd" required>
+          </div>
+          <div class="form-group">
+            <button type="submit">Add Event</button>
+          </div>
+        </form>
+        <button class="close-button" @click="$emit('close')">X</button>
+      </div>
+    </div>
+  </template>
+  
+<script>
+    export default {
+    data() {
+      return {
+        event: {
+          title: '',
+          dateStart: '',
+          dateEnd: '',
+          timeStart: '',
+          timeEnd: ''
+        }
+      }
+    },
+    methods: {
+      submitEvent() {
+        this.$emit('add-event', this.event);
+        this.event.title = '';
+        this.event.date = '';
+        this.event.time = '';
+      },
+      
+    }
+  }
+</script>
+ 
+<style>
+.event-form {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 9999; /* Устанавливаем z-index больше, чем у родительского компонента, чтобы он был поверх него */
+  padding: 20px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+}
+.event-form-modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.event-form-wrapper {
+  background-color: white;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+  display: flex;
+  flex-direction: column;
+}
+
+.event-form {
+  display: flex;
+  flex-direction: column;
+}
+
+.form-group {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+input[type="text"],
+input[type="date"],
+input[type="time"] {
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 10px;
+  font-size: 16px;
+  margin-bottom: 5px;
+}
+
+button[type="submit"] {
+  background-color: #008CBA;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  padding: 10px;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+.close-button {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background-color: white;
+  border: none;
+  font-size: 20px;
+  cursor: pointer;
+  padding: 5px;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  display: flex;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,110 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Qalendar :events="events" @event-was-clicked="handleEventClick" @edit-event="editEvent" @day-was-clicked="showEventForm" :key="calendarKey" @delete-event="deleteEvent"/>
+  <EventForm v-if="showForm" @add-event="addEvent" @close="close"></EventForm>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { Qalendar } from "qalendar";
+import 'qalendar/dist/style.css';
+import EventForm from '@/components/EventForm.vue';
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Qalendar,
+    EventForm
+  },
+  data() {
+    return {
+      events: [
+        {
+          title: "Meeting",
+          time: { start: "2023-03-26 10:00", end: "2023-03-26 11:00" },
+          color: "green",
+          id: "1",
+          isEditable: true,
+        },
+        {
+          title: "Lunch",
+          time: { start: "2023-03-26 12:00", end: "2023-03-26 13:00" },
+          color: "red",
+          id: "2",
+          isEditable: true,
+        }
+      ],
+      showForm: '',
+      eventDate: '',
+      calendarKey: new Date().getTime(),
+      title: '',
+      time: '',
+      color: '',
+      id: '',
+      isEditable: true
+    }
+  },
+  methods: {
+    handleEventClick(event) {
+      console.log(event);
+      console.log(event.clickedEvent.title); // выводим в консоль название события
+      this.title = event.clickedEvent.title;
+      this.time = event.clickedEvent.time;
+      this.color = event.clickedEvent.color;
+      //console.log(`title ${this.title} time ${this.time.start} color ${this.color}`)
+    },
+    editEvent(event) {
+
+      // this.events.forEach(el => {
+      //   if(el.id == event) {
+      //     el.title = 'qwerty'
+      //     console.log("Title " + el.title)
+      //   }
+      // })
+      
+      this.showForm = true;
+    },
+    showEventForm(date) {
+      this.showForm = true;
+      this.eventDate = date;
+      console.log(date);
+      // this.events.push({
+      //   title: 'Test',
+      //   time: { start: "2023-03-26 10:00", end: "2023-03-26 11:00" },
+      //   color: 'green',
+      // });
+      // this.calendarKey = new Date().getTime();
+      // console.log(this.events)
+
+    },
+    addEvent(event) {
+      console.log(event);
+      const title = event.title;
+      const dateStart = event.dateStart;
+      const dateEnd = event.dateEnd;
+      const timeStart = event.timeStart;
+      const timeEnd = event.timeEnd;
+      console.log(title, dateStart, dateEnd, timeStart, timeEnd);
+      this.events.push({
+        title: title,
+        time: { start: `${dateStart} ${timeStart}`, end: `${dateEnd} ${timeEnd}`},
+        color: 'green',
+        id: 3,
+        isEditable: true
+      });
+      this.calendarKey = new Date().getTime();
+      console.log(this.events)
+      this.showForm = false;
+      
+    },
+    deleteEvent(event) {
+      this.events = this.events.filter(el => el.id !== event);
+      console.log(this.events);
+    },
+    close() {
+      this.showForm = false;
+    }
   }
 }
 </script>
+
+<style>
+</style>
